02ec18fed9d490258f6b2a16768d31c34e91faea
Rafael Aguiar Martins <rafael_aguiar@id.uff.br>
2024-06-03 22:56:57 -0300

    commit inicial com models

---
+++ app/Models.hs
@@ -0,0 +1,24 @@
+module Models where
+
+import Data.Set (Set)
+import Data.Tree (Tree)
+
+data Formula = Atomo !Char
+             | Nao !Formula
+             | E !Formula !Formula
+             | Ou !Formula !Formula
+             | Implica !Formula !Formula
+             | Equivale !Formula !Formula
+             deriving (Eq, Ord)
+
+data ArvoreF = Vazia | NoF {
+    formula :: !Formula,
+    esquerda :: !ArvoreF,
+    direita :: !ArvoreF
+} deriving (Eq, Ord)
+
+data StatusConjunto = EhFormula | Aberto | Fechado deriving (Eq, Show)
+
+type TipoNo = (Set Formula, Bool)
+
+type Tableaux = Tree TipoNo
